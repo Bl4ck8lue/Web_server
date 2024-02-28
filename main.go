9b2d3896ee246b0ed59461476fc4c785c235ac7e
Ilya Ilyukhin <ilya.ilyukhin12@yandex.ru>
2024-02-28 22:33:14 +0300

    Create only form and structures

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
+
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type Credentials struct {
@@ -168,7 +166,7 @@ type users struct {
 	Username string `json:"username"`
 }
 
-func Signin(w http.ResponseWriter, r *http.Request) {
+func signinHome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 	html := `
 		<!DOCTYPE html>
@@ -189,52 +187,13 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 	`
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(html))
-	var creds users
-	// Get the JSON body and decode into credentials
-	err := json.NewDecoder(r.Body).Decode(&creds)
-	if err != nil {
-		// If the structure of the body is wrong, return an HTTP error
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Get the expected password from our in memory map
-	expectedPassword, ok := userss[creds.Username]
-
-	// If a password exists for the given user
-	// AND, if it is the same as the password we received, the we can move ahead
-	// if NOT, then we return an "Unauthorized" status
-	if !ok || expectedPassword != creds.Password {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	// Create a new random session token
-	// we use the "github.com/google/uuid" library to generate UUIDs
-	sessionToken := uuid.NewString()
-	expiresAt := time.Now().Add(120 * time.Second)
-
-	// Set the token in the session map, along with the session information
-	sessions[sessionToken] = session{
-		username: creds.Username,
-		expiry:   expiresAt,
-	}
-
-	// Finally, we set the client cookie for "session_token" as the session token we just generated
-	// we also set an expiry time of 120 seconds
-	http.SetCookie(w, &http.Cookie{
-		Name:    "session_token",
-		Value:   sessionToken,
-		Expires: expiresAt,
-	})
-
 }
 
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/based", basedAuth)
 	http.HandleFunc("/logout", logoutHandler)
-	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/sign", signinHome)
 	//http.HandleFunc("/welcome", Welcome)
 	//http.HandleFunc("/logoutcookie", Logout)
 	/*http.HandleFunc("/cookie", cookie)
